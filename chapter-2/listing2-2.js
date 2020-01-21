f3132f2bcf505eb153bdc5cb61f7fac99b4417fe
Robert Hieger <rhieger@gmail.com>
2020-01-21 18:10:46 -0500

    Chapter 2: Completed Listing 2-2
    Created bin in JS Bin
    Downloaded and refactored for use with Node.js

---
+++ chapter-2/listing2-2.js
@@ -0,0 +1,38 @@
+/**
+ * 
+ * Created using JS Bin
+ * http://jsbin.com
+ * 
+ * Copyright (c) 2020 by RHieger (http://jsbin.yutil/3/edit)
+ * 
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+ */
+
+// Getting Programming with JavaScript: Chapter 2
+
+// Code Listing 2.2
+
+var score;
+
+score = 100;
+
+/**
+ * 
+ * MY ADDED CODED FOR DEMO PURPOSES
+ * 
+ */
+
+console.log('\nDECLARED VARIABLES\n')
+
+ declaredVariables = {
+
+    variables: [
+        { "name": "score" }
+    ]
+
+ }; // end declaredVariables()
+
+ console.table(declaredVariables.variables);
+
+console.log('\n');
