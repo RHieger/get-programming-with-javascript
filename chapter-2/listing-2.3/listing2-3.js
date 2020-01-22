fcca6048c6f3b64ee327fa3e44b379967a8f87bc
Robert Hieger <rhieger@gmail.com>
2020-01-22 02:13:28 -0500

    Chapter 2: Listing 2.3 completed.

---
+++ chapter-2/listing-2.3/listing2-3.js
@@ -0,0 +1,37 @@
+/**
+ *
+ * Created using JS Bin
+ * 
+ * http://jsbin.com
+ * 
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/rikelup/2/edit)
+ * 
+ * Released under the MIT license: http://jsbin.mit-license.org 
+ * 
+*/
+
+var score;
+
+score = 100;
+
+/**
+ * 
+ * MY ADDED CODED FOR DEMO PURPOSES
+ * 
+ */
+
+console.log('\nDECLARED VARIABLES\n')
+
+ declaredVariables = {
+
+    variables: [
+        { "name": "score" }
+    ]
+
+ }; // end declaredVariables()
+
+ console.table(declaredVariables.variables);
+
+console.log('\n');
+
+console.log('Current Value of score: ' + score + '\n');
