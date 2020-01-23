e8bcc3c801baf6bfa0961f36a9a00c4a99358d84
Robert Hieger <rhieger@gmail.com>
2020-01-23 00:41:45 -0500

    Chapter 2: Listig 2-8 completed.
    Using a variable’s current value to set its new value

---
+++ chapter-2/listing-2.8/listing-2.8.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * Created using JS Bin
+ * 
+ * http://jsbin.com
+ * 
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/wipuxex/4/edit)
+ * 
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+ */
+
+
+/**
+ *
+ * Chapter 2: Listing 2.8--Using a Variable's Current Value
+ * to Set Its New Value
+ *
+*/
+
+var score;
+
+score = 100;
+
+console.log("\nYour score was " + score + ".\n");
+
+console.log("Great splat!!!\n");
+
+score = score + 50;
+
+console.log("New Score: " + score + "\n");
+
+console.log("Way to go!\n");
