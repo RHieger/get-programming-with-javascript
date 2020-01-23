b8e14254c309bd74acecf77ef9e0c8cbf2abe917
Robert Hieger <rhieger@gmail.com>
2020-01-23 11:42:23 -0500

    Chapter 3: Listing 3.1 complete
    Using variables to represent a book

---
+++ chapter-3/listing-3.1/listing-3.1.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ * Created using JS Bin
+ * 
+ * http://jsbin.com
+ * 
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/honakuv/4/edit)
+ * 
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+ */
+
+
+ /**
+ *
+ * Chapter 3: Listing 3.1--Using Variables to Represent a Book
+ *
+*/
+
+var bookTitle;
+
+var bookAuthor;
+
+bookTitle = 'The Hobbit';
+
+bookAuthor = 'J.R.R. Tolkein';
+
+console.log('\n' + bookTitle + ' by ' + bookAuthor + '\n');
