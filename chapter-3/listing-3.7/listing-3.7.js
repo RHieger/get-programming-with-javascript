982e6b1334dd86a826c2050c359f723272d63c8d
Robert Hieger <rhieger@gmail.com>
2020-01-24 00:59:12 -0500

    Chapter 3: Listing 3.7 completed.
    Objects with multiple properties

---
+++ chapter-3/listing-3.7/listing-3.7.js
@@ -0,0 +1,49 @@
+/**
+ * 
+ * Created using JS Bin
+ * 
+ * http://jsbin.com
+ * 
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/nawanac/3/edit)
+ * 
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+ */
+
+/**
+ *
+ * Chapter 3: Listing 3.7--Objects with Multiple Properties
+ *
+*/
+
+var book1;
+
+var book2;
+
+book1 = {
+  
+  title: "The Hobbit",
+  author: "J.R.R. Tolkein"
+  
+};  // end book1()
+
+
+book2 = {
+  
+  title: "Northern Lights",
+  author: "Philip Pullman"
+  
+};  // end book2()
+
+
+console.log('\nbook1:');
+
+console.log(book1);
+
+console.log('\n');
+
+console.log('book2:');
+
+console.log(book2);
+
+console.log('\n');
