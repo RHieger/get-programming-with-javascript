40c078473a6b36bff2d8dc88f3c692b9e8a6857b
Robert Hieger <rhieger@gmail.com>
2020-01-23 19:13:50 -0500

    Chapter 3: Completed Listing 3.2.
    Distinguishing similar variables by prepending prefixes.

---
+++ chapter-3/listing-3.2/listing-3.2.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * Created using JS Bin
+ * 
+ * http://jsbin.com
+ * 
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/dorinaz/6/edit)
+ * 
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+ */
+
+
+/**
+ *
+ * Chapter 3: Listing 3.2--Using Prefixes to Tell Book Variables Apart
+ *
+*/
+
+var book1Title = "The Hobbit";
+var book1Author = "J.R.R. Tolkein";
+
+var book2Title = "Northern Lights";
+var book2Author = "Philip Pullman";
+
+var book3Title = "The Adventures of Tom Sawyer";
+var book3Author = "Mark Twain";
+
+console.log('\nThere are only 3 books so far...\n');
+
+console.log(book1Title.toUpperCase() + ' by ' + book1Author);
+console.log(book2Title.toUpperCase() + ' by ' + book2Author);
+console.log(book3Title.toUpperCase() + ' by ' + book3Author + '\n');
