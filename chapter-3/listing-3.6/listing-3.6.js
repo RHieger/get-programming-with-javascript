62fcd468335931edc1496784ecbf7c6082110211
Robert Hieger <rhieger@gmail.com>
2020-01-24 00:38:23 -0500

    Chapter 3: Completed Listing 3.6.
    Using a variable as a property value.

---
+++ chapter-3/listing-3.6/listing-3.6.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ * Created using JS Bin
+ * 
+ * http://jsbin.com
+ * 
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/gegotey/5/edit)
+ * 
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+ */
+
+/**
+ *
+ * Chapter 3: Listing 3.6--Using a Variable as a Property Value
+ *
+*/
+
+var book;
+
+var bookName;
+
+bookName = "The Adventures of Tom Sawyer";
+
+book = {
+  
+ title: bookName 
+  
+};  // end book()
+
+console.log('\nbook:');
+
+console.log(book);
+
+console.log('\n');
