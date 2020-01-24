136d56a72f5c53bc2c4a20eedea1cdb62b72caaa
Robert Hieger <rhieger@gmail.com>
2020-01-23 20:16:43 -0500

    Chapter 3: Listing 3.4 completed.
    Creating an empty object.

---
+++ chapter-3/listing-3.4/listing-3.4.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ * Created using JS Bin
+ * 
+ * http://jsbin.com
+ * 
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/miyusic/3/edit)
+ * 
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+ */
+
+/**
+ *
+ * Chapter 3: Listing 3.4--Creating An Empty Object
+ *
+*/
+
+var book;
+
+console.log('\nInitial Value of book variable: ', book, '\n');
+
+book = {};
+
+console.log('Type of book After Assignment: ', typeof book);
+
+console.log('Properties of book After Assignment: ', book, '\n');
