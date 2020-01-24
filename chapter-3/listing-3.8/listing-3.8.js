48e6ec2d9e3e99e8288559f8a70d16a227ed7382
Robert Hieger <rhieger@gmail.com>
2020-01-24 15:26:46 -0500

    Chapter 3: Listing 3-8 complete.
    Using dot. otation to access property values

---
+++ chapter-3/listing-3.8/listing-3.8.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * Created using JS Bin
+ * 
+ * http://jsbin.com
+ * 
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/hiyekas/4/edit)
+ * 
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+ */
+
+/**
+ *
+ * Chapter 3: Listing 3.8--Using Dot Notation to Access Property Values
+ *
+*/
+
+var book;
+
+book = {
+  
+      title: "The Hobbit",
+     author: "J.R.R. Tolkein",
+  published: 1937
+  
+};  // end book()
+
+console.log('\nEXTRACT PROPERTIES OF book OBJECT:\n');
+
+console.log('Book Title:', book.title, '\n');
+
+console.log('Author:', book.author, '\n');
