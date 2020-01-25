4f9630917f181a376674da1d278ba5524f4da34b
Robert Hieger <rhieger@gmail.com>
2020-01-25 10:01:58 -0500

    Chapter 3: Listing 3.12 completed.
    A blog post (Object)

---
+++ chapter-3/listing-3.12/listing-3.12.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ * Created using JS Bin
+ * 
+ * http://jsbin.com
+ * 
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/cesodat/7/edit)
+ * 
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+ */
+
+/**
+ *
+ * Get Programming with JavaScript
+ *
+ * Chapter 3: Listing 3.12--A Blog Post (Object)
+ *
+*/
+
+
+var post = {
+  
+  id: 1,
+  title: "My Crazy Space Adventure",
+  author: "Philae",
+  created: "2015-06-21",
+  body: `You will not believe where I just woke up!!
+  Only on a comet...`
+  
+};  // end post()
+
+console.log('\npost OBJECT--EXAMPLE REPRESENTING BLOG POST:\n');
+
+console.log(post, '\n');
