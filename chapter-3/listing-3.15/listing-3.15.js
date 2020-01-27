f5613c69e805cc05f020681f82385a5e8c294e5d
Robert Hieger <rhieger@gmail.com>
2020-01-27 01:07:34 -0500

    Chapter 3: Listing 3.15 completed.
    Object modeling quiz question and answer

---
+++ chapter-3/listing-3.15/listing-3.15.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ * Created using JS Bin
+ * 
+ * http://jsbin.com
+ * 
+ * Copyright (c) 2020 By RHieger (http://jsbin.com/yicebiq/9/edit)
+ * 
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+ */
+
+/**
+ *
+ * Get Programming with JavaScript
+ *
+ * Chapter 3: Listing 3.15--A Question and Answer for a Quiz App
+ *
+*/
+
+var questionAndAnswer = {
+  
+  question        : "What is the capital of France?",
+  answer1         : "Bordeaux",
+  answer2         : "F",
+  answer3         : "Paris",
+  answer4         : "Brussels",
+  correctAnswer   : "Paris",
+  marksForQuestion: 2
+  
+};  // end questionAndAnswer()
+
+console.log('\nSAMPLE OBJECT MODELING A QUIZ QUESTION AND ANSWER:\n');
+
+console.log(questionAndAnswer);
+
+console.log('\n');
