98c2a1cd9fe936fd00bfccbd3f462bdc04a812f3
Robert Hieger <rhieger@gmail.com>
2020-01-31 12:34:44 -0500

    Chapter 4: Listing 4.4 completed.
    Simple function definition and assignment.

---
+++ chapter-4/listing-4.4/listing-4.4.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ * Created using JS Bin
+ * 
+ * http://jsbin.com
+ * 
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/viwufon/2/edit)
+ * 
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+ */
+
+/**
+ *
+ * Get Programming with JavaScript
+ *
+ * Chapter 4: Listing 4.4--A Simple Function Definition and Assignment
+ *
+*/
+
+var sayHello;
+
+sayHello = function()  {
+
+  console.log('Hello World!');
+
+} // end sayHello()
+
+// SCRIPT STRUCTURE:
+
+console.log('\nFUNCTION LISTING:');
+
+console.log('----------------\n')
+
+console.log(sayHello.toString() + '\n\n');
+
+console.log('FUNCTION OUTPUT:');
+
+console.log('---------------\n');
+
+sayHello();
+
+console.log('\n');
