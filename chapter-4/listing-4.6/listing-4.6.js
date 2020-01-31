c370b9dbfebf0cc33e409ceda42ad99b9eaab2ee
Robert Hieger <rhieger@gmail.com>
2020-01-31 16:43:27 -0500

    Chapter 4: Listing 4.6 completed.
    Call sayHello() function 3 times in a row.

---
+++ chapter-4/listing-4.6/listing-4.6.js
@@ -0,0 +1,47 @@
+/**
+ * 
+ * Created using JS Bin
+ * 
+ * http://jsbin.com
+ * 
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/paqabem/5/edit)
+ * 
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+ */
+
+
+ /**
+ *
+ * Get Programming with JavaScript
+ *
+ * Chapter 4: Listing 4.6--Calling the sayHello() Function 3 Times
+ *
+*/
+
+
+var sayHello;
+
+sayHello = function()  {
+  
+  
+  console.log("Hello World!");
+  
+  
+};  // end sayHello()
+
+console.log('\nLISTING OF sayHello() FUNCTION:');
+
+console.log('------------------------------\n\n');
+
+console.log(sayHello.toString() + '\n\n');
+
+console.log('CALL sayHello() FUNCTION 3 TIMES:');
+
+console.log('--------------------------------\n\n');
+
+sayHello();
+sayHello();
+sayHello();
+
+console.log('\n\n');
