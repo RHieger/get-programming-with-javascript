d762e7731d57c1e27d313823af40683758f651c5
Robert Hieger <rhieger@gmail.com>
2020-02-08 10:59:02 -0500

    Chapter 5: Listing 5.7 completed.
    Displaying a player’s name

---
+++ chapter-5/listing-5.7/listing-5.7.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ * Created using JS Bin
+ * 
+ * http://jsbin.com
+ * 
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/teyusom/5/edit)
+ * 
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+*/
+
+
+/**
+ *
+ * Get Programming with JavaScript
+ *
+ * Chapter 5: Listing 5.7--Displaying a Player's Name
+ *
+*/
+
+var showPlayerName;
+
+showPlayerName = function(playerName)  {
+  
+  console.log('playerName: ' + playerName);
+  
+};  // end showPlayerName()
+
+console.log('\n\nLISTING OF showPlayerName():');
+console.log('---------------------------\n\n');
+console.log(showPlayerName.toString() + '\n\n');
+
+// 2 SAMPLE CALLS TO showPlayerName() FUNCTION:
+
+console.log('2 SAMPLE CALLS TO showPlayerName():');
+console.log('----------------------------------\n');
+
+showPlayerName('Kandra');
+
+showPlayerName('Dax');
+
+console.log('\n\n');
