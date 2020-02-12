542e5bc7dabe3b40a2bf3ccd9f02ccf783538d76
Robert Hieger <rhieger@gmail.com>
2020-02-12 18:20:34 -0500

    Chapter 6: Listing 6.7 completed.
    Getting a string for a player’s name

---
+++ chapter-6/listing-6.7/listing-6.7.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ * Created using JS Bin
+ * http://jsbin.com
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/lepufok/3/edit)
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+*/
+
+
+/**
+ *
+ * Get Programming with JavaScript
+ *
+ * Chapter 6: Listing 6.7--Getting a String for a Player's Name
+ *
+*/
+
+var getPlayerName;
+
+getPlayerName = function(playerName)  {
+  
+  return playerName;
+  
+};  // end getPlayerName()
+
+console.log('\nLISTING FOR getPlayerName():');
+console.log('-----------------------\n');
+console.log(getPlayerName.toString() + '\n\n');
+
+console.log('PLAYER NAME: ' + getPlayerName('Kandra') + '\n\n');
