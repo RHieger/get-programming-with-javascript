6b2b5294ef43b5cf5ba368dd9bfb6c56f75ce3c8
Robert Hieger <rhieger@gmail.com>
2020-02-16 17:09:18 -0500

    Chapter 7: Listing 7.7 completed.
    Converting a string to uppercase

---
+++ chapter-7/listing-7.7/listing-7.7.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * Created using JS Bin
+ * http://jsbin.com
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/bibujax/4/edit)
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+*/
+
+/**
+ *
+ * Get Programming with JavaScript
+ *
+ * Chapter 7: Listing 7.7--Converting a String to Uppercase
+ *
+*/
+
+var planet = 'Jupiter';
+
+var bigPlanet = planet.toUpperCase();
+
+console.log('\nplanet: ' + planet + '\n\n');
+
+console.log('AFTER planet USES STRING METHOD toUpperCase():');
+console.log('=============================================\n\n');
+
+console.log(planet + ' becomes ' + bigPlanet);
+
+console.log('\n\n');
