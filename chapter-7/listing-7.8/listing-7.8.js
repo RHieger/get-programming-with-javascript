0cf8090008166609e93ef458d72f816a23a31c85
Robert Hieger <rhieger@gmail.com>
2020-02-17 14:57:21 -0500

    Chapter 7: Listing 7.8 completed.
    Finding substrings

---
+++ chapter-7/listing-7.8/listing-7.8.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * Created using JS Bin
+ * http://jsbin.com
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/norifaj/3/edit)
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+*/
+
+/**
+ *
+ * Get Programming with JavaScript
+ *
+ * Chapter 7: Listing 7.8--Finding Substrings
+ *
+*/
+
+var message = "We choose to go to the Moon!";
+
+console.log('\n\nmessage = ' + message + '\n');
+
+console.log( message.substr(3, 12) );
