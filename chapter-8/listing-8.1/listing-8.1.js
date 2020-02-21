abe9a59e9af10a1980725e3693a77def939eb4cc
Robert Hieger <rhieger@gmail.com>
2020-02-21 13:45:23 -0500

    Chapter 8: Listing 8.1 completed.
    Creating arrays

---
+++ chapter-8/listing-8.1/listing-8.1.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * Created using JS Bin
+ * http://jsbin.com
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/cuqedor/6/edit)
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+*/ 
+
+
+/**
+ *
+ * Get Programming with JavaScript
+ *
+ * Chapter 8: Listing 8.1--Creating Arrays
+ *
+*/
+
+var scores;
+
+var names;
+
+console.log('\nELEMENTS IN scores[]:');
+console.log('====================\n');
+
+scores = [3, 1, 8, 2];
+
+names = ['Kandra', 'Dax', 'Blinky'];
+
+console.table(scores);
+
+console.log('\n');
+
+console.log('ELEMENTS IN names[]:');
+console.log('===================\n');
+
+console.table(names);
+
+console.log('\n');
