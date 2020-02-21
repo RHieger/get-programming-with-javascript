8f7197b1e30031e2a154ef674c0b3c7c3455540d
Robert Hieger <rhieger@gmail.com>
2020-02-21 14:20:43 -0500

    Chapter 8: Listing 8.2 completed.
    Using existing objects in an array

---
+++ chapter-8/listing-8.2/listing-8.2.js
@@ -0,0 +1,41 @@
+/**
+ * 
+ * Created using JS Bin
+ * http://jsbin.com
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/henoday/8/edit)
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+*/
+
+
+/**
+ *
+ * Get Programming with JavaScript
+ *
+ * Chapter 8: Listing 8.2--Using Existing Objects in an Array
+ *
+*/
+
+var place1 = { name: "The Pyramids", country: "Egypt" };
+
+var place2 = { name: "The Grand Canyon", country: "USA" };
+
+var place3 = { name: "Bondi Beach", country: "Australia" };
+
+var thisYear = [place1, place2];
+
+var nextYear = [place3];
+
+console.log('\nELEMENTS IN thisYear[]:');
+console.log('----------------------\n');
+
+console.log(thisYear);
+
+console.log('\n');
+
+console.log('ELEMENTS IN nextYear[]:');
+console.log('----------------------\n');
+
+console.log(nextYear);
+
+console.log('\n');
