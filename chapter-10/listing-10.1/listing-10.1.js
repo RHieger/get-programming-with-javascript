07326319a76009b16ffd247317989fe39c1de168
Robert Hieger <rhieger@gmail.com>
2020-03-16 07:41:19 -0400

    Chapter 10: Listing 10.1 conmpleted.
    Bracket notation for object properties

---
+++ chapter-10/listing-10.1/listing-10.1.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * Created using JS Bin
+ * http://jsbin.com
+ * Copyright (c) 2020 by RHieger (http://jsbin.com/vebacon/5/edit)
+ * Released under the MIT license: http://jsbin.mit-license.org
+ * 
+*/
+
+/**
+ *
+ * Get Programming with JavaScript
+ *
+ * Listing 10.1--Bracket Notation for Object Properties
+ *
+*/
+
+var ages = {};
+
+// Set object properties using bracket notation:
+
+ages['Kandra'] = 56;
+
+ages['Dax'] = 21;
+
+// Get the properties using dot notation and
+// the names as keys:
+
+console.log('\n');
+
+console.log('ages.Kandra: ' + ages.Kandra + '\n');
+
+console.log('ages.Dax: ' + ages.Dax + '\n\n');
